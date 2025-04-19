f8c0b342981a645e7183c9d1fbc6cf09137d07a1
zaheer-2005 <zaheerimbraan@gmail.com>
2025-04-19 11:45:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 
-module.exports = {
+module.exports = {VAJIRA-MD=xGV31ZZb#AcuwZTIG38JWXTavMKNJPmrnD7_f56NMKBcNuXHP-tM
 SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=eFdzCSbY#dye5BjG3uRjo81YZzxltr2CsrQY45oZLC4Txzq3zBtI' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: +94 78 894 5581.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
